da13ad030d68e074987b3e81e90eb9eb938cafdc
MpagiRaymond <raympagi@gmail.com>
2025-01-08 18:27:38 +0300

    having multile pages

--- src/client-panel/cards.jsx
+++ src/client-panel/cards.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-import {jo} from '../assets/jo';
+import React, {useEffect, useState} from "react";
+import logo from '../assets/jo';
+
 function cards() {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        alert(`Thanks for voting. Total votes ${count}`);
+    });
+    const leaders = ['Vice president', 'Chief prefect', 'Sports prefect'];
 render(
     <>
     <div className="cards">
-        <img src="{jo}" alt="image" />
-        <p className="title">Vice president</p>
+        <img src={logo} alt="image" />
+        <p className="title">{leaders.map(leader => leader)}</p>
         <div className="Details">
             <div>Name: kijjambu Mark</div>
             <div>Class: P7</div>
         </div>
+        <button onClick={() => setCount(count + 1)}>VOTE</button>
     </div>  
     </>
 );
