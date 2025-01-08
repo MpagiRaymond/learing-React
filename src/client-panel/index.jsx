c19eb1ccbb0b66f186834e4b07312dca51435fbb
MpagiRaymond <raympagi@gmail.com>
2025-01-08 18:33:15 +0300

    having multile pages

---
+++ src/client-panel/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import cards from './cards'
+
+const Apps = () => {
+    return (
+        <>
+        <header />
+        <main>
+            <cards />
+            <cards/>
+        </main>
+        <footer />
+        </>
+    );
+}
+
+export default Apps;
